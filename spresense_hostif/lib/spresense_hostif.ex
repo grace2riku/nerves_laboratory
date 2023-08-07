3bb6bbf64c3633a834cbdf10abf1c94fcaabd370
Koji Abe <gracehime@gmail.com>
2023-08-08 08:31:32 +0900

    mix nerves.new でSpresense Host IF確認用プロジェクトを作成した。

---
+++ spresense_hostif/lib/spresense_hostif.ex
@@ -0,0 +1,18 @@
+defmodule SpresenseHostif do
+  @moduledoc """
+  Documentation for SpresenseHostif.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpresenseHostif.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ spresense_hostif/test/spresense_hostif_test.exs
@@ -0,0 +1,8 @@
+defmodule SpresenseHostifTest do
+  use ExUnit.Case
+  doctest SpresenseHostif
+
+  test "greets the world" do
+    assert SpresenseHostif.hello() == :world
+  end
+end
