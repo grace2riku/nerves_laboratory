8a3ff42ee12594285fbde195095a4177ea0a958b
Koji Abe <gracehime@gmail.com>
2023-08-13 16:09:20 +0900

    チャンネル指定(0-7)で電圧を取得できるようにした。測定はsingle_ended固定。
    # 実行例
    iex(14)> AdcMcp3208.read_voltage(7)
    07:08:26.968 [info] Channel = 7, ADC count = 2113, Voltage = 1.7027838827838828
    1.7027838827838828
    
    iex(15)> AdcMcp3208.read_voltage(0)
    07:08:37.669 [info] Channel = 0, ADC count = 2112, Voltage = 1.701978021978022
    1.701978021978022

--- adc_mcp3208/lib/adc_mcp3208.ex
+++ adc_mcp3208/lib/adc_mcp3208.ex
@@ -17,15 +17,19 @@ defmodule AdcMcp3208 do
   end
 
   require Logger
-  def read_voltage() do
+  @start_bit 1
+  def read_voltage(channel) do
+    # 1 = single_ended, 0 = differential
+    single_ended = 1
+
     # 0chの電圧値を取得
     {:ok, ref} = Circuits.SPI.open("spidev0.0")
-    {:ok, <<_::size(12), adc_counts::size(12)>>} = Circuits.SPI.transfer(ref, <<0x06, 0x00, 0x00>>)
+    {:ok, <<_::size(12), adc_counts::size(12)>>} = Circuits.SPI.transfer(ref, <<0::size(5), @start_bit::size(1), single_ended::size(1), channel::size(3), 0::size(6), 0x00>>)
 
     voltage = adc_counts / 4095 * 3.3
 
     # 測定結果を表示
-    Logger.info("ADC count: #{adc_counts}, Voltage: #{voltage}")
+    Logger.info("Channel = #{channel}, ADC count = #{adc_counts}, Voltage = #{voltage}")
 
     Circuits.SPI.close(ref)
 
