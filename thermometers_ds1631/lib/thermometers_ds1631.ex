f64ec790ff65d91941f42f05c64cc401568e20e9
Koji Abe <gracehime@gmail.com>
2023-08-13 16:51:19 +0900

    温度センサDS1631の確認用プロジェクトを作成した。 実行したコマンドは下記。
    $ mix nerves.new thermometers_ds1631

---
+++ thermometers_ds1631/lib/thermometers_ds1631.ex
@@ -0,0 +1,18 @@
+defmodule ThermometersDs1631 do
+  @moduledoc """
+  Documentation for ThermometersDs1631.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThermometersDs1631.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ thermometers_ds1631/test/thermometers_ds1631_test.exs
@@ -0,0 +1,8 @@
+defmodule ThermometersDs1631Test do
+  use ExUnit.Case
+  doctest ThermometersDs1631
+
+  test "greets the world" do
+    assert ThermometersDs1631.hello() == :world
+  end
+end
