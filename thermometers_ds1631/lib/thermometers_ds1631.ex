01b2a24d2cb37ad0ace300f90cda9c366aca418c
Koji Abe <gracehime@gmail.com>
2023-08-14 12:22:08 +0900

    ds1631の温度取得関数を実装した。

--- thermometers_ds1631/lib/thermometers_ds1631.ex
+++ thermometers_ds1631/lib/thermometers_ds1631.ex
@@ -15,4 +15,33 @@ defmodule ThermometersDs1631 do
   def hello do
     :world
   end
+
+  require Logger
+  alias Circuits.I2C
+  @i2c_bus "i2c-1"
+  @ds1631_i2c_addr 0x48
+  @conversion_time 500
+
+  @start_convert_command 0x51
+  @read_temperature_command 0xAA
+  @stop_convert_command 0x22
+
+  def read_temperature do
+    {:ok, ref} = I2C.open(@i2c_bus)
+
+    I2C.write(ref, @ds1631_i2c_addr, <<@start_convert_command>>)
+    Process.sleep(@conversion_time)
+    I2C.write(ref, @ds1631_i2c_addr, <<@read_temperature_command>>)
+
+    {:ok, <<raw_temperature>>} = I2C.read(ref, @ds1631_i2c_addr, 2)
+
+    I2C.write(ref, @ds1631_i2c_addr, <<@stop_convert_command>>)
+    I2C.close(ref)
+
+    temperature = raw_temperature / 256.0
+    Logger.info("Raw temperature = #{raw_temperature}, Temperature = #{temperature}")
+
+    temperature
+  end
+
 end
