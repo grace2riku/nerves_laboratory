96745589ab7fb2590fef1896ca3ca70693ab6882
Koji Abe <gracehime@gmail.com>
2023-08-15 00:46:00 +0900

    バッファサイズ取得コマンドでバッファ0〜4で期待値を取得できた。 バッファ0〜3はspresenseのサンプルプログラムの設定値とおり。バッファ4は未設定のため0でOK。 以下は実行結果のログ。
    iex(2)> SpresenseHostif.get_bufsize(0)
    {:ok, 256}
    
    15:39:25.244 [info] cmd_result = 0, Buffer id = 0, Buffer size = 256
    iex(3)> SpresenseHostif.get_bufsize(1)
    
    15:39:35.358 [info] cmd_result = 0, Buffer id = 1, Buffer size = 256
    {:ok, 256}
    iex(4)> SpresenseHostif.get_bufsize(2)
    
    15:39:38.193 [info] cmd_result = 0, Buffer id = 2, Buffer size = 51
    {:ok, 51}
    iex(5)> SpresenseHostif.get_bufsize(3)
    
    15:39:42.972 [info] cmd_result = 0, Buffer id = 3, Buffer size = 8
    {:ok, 8}
    iex(6)> SpresenseHostif.get_bufsize(4)
    
    15:42:45.779 [info] cmd_result = 0, Buffer id = 4, Buffer size = 0
    {:ok, 0}

--- spresense_hostif/lib/spresense_hostif.ex
+++ spresense_hostif/lib/spresense_hostif.ex
@@ -15,4 +15,25 @@ defmodule SpresenseHostif do
   def hello do
     :world
   end
+
+  require Logger
+  alias Circuits.SPI
+  @icmd_available_size_id 0x10
+
+  def get_bufsize(buffer_id) do
+    {:ok, ref} = SPI.open("spidev0.0", mode: 1, speed_hz: 800000)
+    icmd_available_cmd = @icmd_available_size_id + buffer_id
+
+    {:ok, <<_::16, cmd_result::8, bufsize::16-little>>} = SPI.transfer(ref, <<icmd_available_cmd, 0xff, 0xff, 0xff, 0xff>>)
+
+    Circuits.SPI.close(ref)
+
+    Logger.info("cmd_result = #{cmd_result}, Buffer id = #{buffer_id}, Buffer size = #{bufsize}")
+
+    case cmd_result do
+      0 -> {:ok, bufsize}
+      _ -> {:error, 0}
+    end
+  end
+
 end
